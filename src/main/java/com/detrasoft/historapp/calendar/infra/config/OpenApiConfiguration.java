c12f745082666c3674810e587389a689eb29d412
Michael Souto <michaelsouto@outlook.com>
2023-05-08 22:41:47 -0300

    fix errors

---
+++ src/main/java/com/detrasoft/historapp/calendar/infra/config/OpenApiConfiguration.java
@@ -0,0 +1,31 @@
+package com.detrasoft.historapp.calendar.infra.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(info =
+@Info(title = "Calendar Service API",
+        version = "v1",
+        description = "Documentation of Calendar Service API"))
+public class OpenApiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(
+                        new io.swagger.v3.oas.models.info.Info()
+                                .title("Calendar Service API")
+                                .version("v1")
+                                .license(
+                                        new License()
+                                                .name("Apache 2.0")
+                                                .url("http://springdoc.org")
+                                )
+                );
+    }
+}
